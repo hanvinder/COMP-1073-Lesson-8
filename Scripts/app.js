7d16efc3f12f109ef58addc1d283fdb66e5e0e88
hanvinder <hanvinder@gmail.com>
2016-03-10 10:21:50 -0500

    udpate js file

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,15 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
+	
 "use strict";
+
+var myVariable = "hello";
+console.log("myVariable"+ myVariable);
+
+var myArray =[];
+
+myArray.push("my fiirst greeting");
+myArray.push("my second greeting");
+console.log(myArray[1]);
 
 })();
