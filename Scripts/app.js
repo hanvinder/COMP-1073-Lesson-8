e5e10739d54df87261c4f4a6bacc1f8b666c8cc3
hanvinder <hanvinder@gmail.com>
2016-03-10 10:30:13 -0500

    adding array

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,11 +5,31 @@
 
 var myVariable = "hello";
 console.log("myVariable"+ myVariable);
-
+/*
+var myArray =[
+	"my fiirst greeting",
+	"my second greeting"
+];*/
+ 
+/*
 var myArray =[];
 
 myArray.push("my fiirst greeting");
 myArray.push("my second greeting");
-console.log(myArray[1]);
+
+
+console.log(myArray[1]);*/
+var myArray =[
+	"Hanvinder",
+	"360 DUCKWORTH",
+	"Barrie",
+	"Ontario",
+	20,
+	true
+];
+
+for (var index=0; index < myArray.lengh; index++){
+	console.log(myArray[index]);
+}
 
 })();
